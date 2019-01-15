019d01ac753a114f107a147c896c81eb0e4d1d5f
Willi Krappen <wkrappen91@yahoo.de>
2019-01-15 14:39:35 +0100

    initial component

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
-import CSSModules from 'react-css-modules';
-import style from './header.less';
+import React from "react";
+import CSSModules from "react-css-modules";
+import style from "./header.less";
 
-@CSSModules(style, {allowMultiple:true, handleNotFoundStyleName:'log'})
-export default class Header extends React.Component{
-	render(){
-		return (<div styleName="header">Header</div>);
+@CSSModules(style, { allowMultiple: true, handleNotFoundStyleName: "log" })
+export default class Header extends React.Component {
+	constructor(p) {
+		super(p);
+		this.state = { color: "red" };
+	}
+	clicked() {
+		this.setState({ color: this.state.color == "red" ? "blue" : "red" });
+	}
+	render() {
+		return (
+			<div
+				onClick={() => {
+					this.clicked();
+				}}
+				styleName="header"
+				style={{ background: this.state.color }}
+			>
+				Header
+			</div>
+		);
 	}
 }
