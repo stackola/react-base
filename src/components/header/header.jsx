20a468a379eb7ecd36814667465b788b9379cf2f
Willi Krappen <wkrappen91@yahoo.de>
2019-01-15 14:43:58 +0100

    header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -6,9 +6,11 @@ import style from "./header.less";
 export default class Header extends React.Component {
 	constructor(p) {
 		super(p);
+		//this only works in the constructor:
 		this.state = { color: "red" };
 	}
 	clicked() {
+		//this is how to set the state outside of the constructor
 		this.setState({ color: this.state.color == "red" ? "blue" : "red" });
 	}
 	render() {
@@ -20,7 +22,8 @@ export default class Header extends React.Component {
 				styleName="header"
 				style={{ background: this.state.color }}
 			>
-				Header
+				Header<br/>
+				{this.state.color}
 			</div>
 		);
 	}
